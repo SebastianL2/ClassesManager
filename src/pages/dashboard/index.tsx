ff57c359d13191f433f7d14d0e85e91dd175bff2
SebastianL2 <sebas16cely@gmail.com>
2024-05-30 23:48:27 -0500

    topbar

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Sidebar } from '../sidebar';
+
 import { ColorModeContext, useMode } from '../../theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { Topbar } from '../general/top-bar';
@@ -10,7 +10,7 @@ return(
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline/>
-        <Sidebar/>
+        
         <div className='app'>
           <main className='content'>
            <Topbar/>
