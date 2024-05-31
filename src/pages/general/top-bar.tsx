014babafeec16b833074f28dff13eeb09357d613
SebastianL2 <sebas16cely@gmail.com>
2024-05-31 00:03:33 -0500

    topbar finish visaul aspects

--- src/pages/general/top-bar.tsx
+++ src/pages/general/top-bar.tsx
@@ -1,6 +1,6 @@
 import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
-import { ColorModeContext, palette } from "../../theme";
+
 import InputBase from "@mui/material/InputBase";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
@@ -8,6 +8,7 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
+import { ColorModeContext, palette } from "../../theme";
 
 
 export const Topbar: React.FC =()=>{
@@ -18,12 +19,18 @@ export const Topbar: React.FC =()=>{
   return (
     <Box display="flex" justifyContent="space-between" p={2}>
     
-  
+    <Box
+      display="flex"
+      sx={{
+        border: `2px solid ${colors.primary[400]}`,
+      }}
+      borderRadius="3px"
+    >
       <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
       <IconButton type="button" sx={{ p: 1 }}>
         <SearchIcon />
       </IconButton>
-
+    </Box>
 
 
     <Box display="flex">
